cfbd0704ae1cd70feb657b72397cb1f2c35df752
K0LESAN <k0lesan2k00@gmail.com>
2025-05-18 00:29:57 +0300

    refactor: move hooks to new file

--- src/store/type.ts
+++ src/store/type.ts
@@ -1,12 +1,6 @@
-import { useDispatch, useSelector } from 'react-redux';
 import type store from '.';
 
 type RootState = ReturnType<typeof store.getState>;
 type AppDispatch = typeof store.dispatch;
 
-const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-const useAppSelector = useSelector.withTypes<RootState>();
-
 export type { RootState, AppDispatch };
-
-export { useAppDispatch, useAppSelector };
