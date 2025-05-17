bd1dba94a58121ba7c88b7abd7ea2326cf8fec9d
K0LESAN <k0lesan2k00@gmail.com>
2025-05-18 00:22:49 +0300

    feat: add provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { StrictMode } from 'react';
+import store from './store/index.ts';
 import App from './app.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
